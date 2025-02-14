e9bab0b98f9a801c201b7fa5c4bea29cbbbd5a2c
Onkar Kapuskari <on.kaps18@gmail.com>
2025-02-14 22:50:49 +0530

    add few stylistic rule for testing

--- eslint.config.js
+++ eslint.config.js
@@ -1,14 +1,75 @@
+import stylisticJs from '@stylistic/eslint-plugin-js';
+import stylistic from '@stylistic/eslint-plugin';
+
 export default [
   {
+    "plugins": {
+      "@stylistic/js": "stylisticJs",
+      "@stylistic": "stylistic"
+    },
     "rules": {
       "indent": [
         "error",
         4,
-        "tab",
         {
-          "SwitchCase": 5
+          "SwitchCase": 5,
+          "FunctionDeclaration": {
+            "parameters": 3,
+            "body": 2
+          }
         }
-      ]
+      ],
+      "quotes": {
+        "@stylistic/quotes": [
+          "error",
+          "double"
+        ],
+        "quotes": [
+          "error",
+          "double"
+        ]
+      },
+      "semi": {
+        "@stylistic/semi": [
+          "error",
+          true
+        ],
+        "semi": [
+          "error",
+          true
+        ]
+      },
+      "max-len": {
+        "max-len": [
+          "error",
+          120
+        ],
+        "@stylistic/max-len": [
+          "error",
+          120
+        ]
+      },
+      "array-bracket-newline": {
+        "array-bracket-newline": [
+          "error",
+          "always"
+        ],
+        "@stylistic/js/array-bracket-newline": [
+          "error",
+          "always"
+        ]
+      },
+      "array-element-newline": {},
+      "no-trailing-spaces": {
+        "@stylistic/no-trailing-spaces": [
+          "error",
+          false
+        ],
+        "no-trailing-spaces": [
+          "error",
+          false
+        ]
+      }
     }
   }
 ];
--- src/plugins/lint/eslint.lua
+++ src/plugins/lint/eslint.lua
@@ -12,7 +12,7 @@ end
 function Validate(config)
     -- sleep(2000)
     local temp = { foo = 'bar', baz = 123, qux = { nested = true } }
-    log.debug(temp);
+    log.debug(config);
     return true
 end
 
@@ -21,8 +21,31 @@ function Generate(config)
     log.info("Generating ESLint configuration")
     -- log.debug(common)
 
+    --Import necessary plugins
     local imports = {}
     local plugins = {}
+    --Default Stylistic Plugin
+    imports["stylistic"] = '@stylistic/eslint-plugin'
+    plugins["@stylistic"] = "stylistic"
+    
+    --Function to get correct stylistic plugin based on rule extension
+    local function getplugin(rule_key)
+        local extension_map = {
+            js = "stylisticJs",
+            ts = "stylisticTs",
+        }
+        local lang_ext = rule_key:match("_(%a+)$") -- Extract '_js' or '_ts'
+        if lang_ext and extension_map[lang_ext] then
+            local plugin_name = extension_map[lang_ext]
+            if not imports[plugin_name] then
+                imports[plugin_name] = "@stylistic/eslint-plugin-".. lang_ext
+                plugins["@stylistic/" .. lang_ext] = plugin_name
+            end
+            return "@stylistic/" .. lang_ext
+        end
+        return "@stylistic"
+    end
+    
 
     local function getIndentRule(indent_config)
 
@@ -30,11 +53,8 @@ function Generate(config)
             return nil
         end
 
-        imports["stylisticJs"] = '@stylistic/eslint-plugin-js'
-        plugins["@stylistic/js"] = "stylisticJs"   
-
-        local indentation = common.indent.indent_style
-        local indent_size = common.indent.indent_size
+        local indentation = indent_config.indent_style
+        local indent_size = indent_config.indent_size
         -- Base indentation configuration
         local indentOptions = {
             "error",
@@ -45,92 +65,117 @@ function Generate(config)
         local options = {}
 
         -- Handle additional indent-specific options if they exist in config
-        if common.indent.switch_case then
-            options.SwitchCase = common.indent.switch_case
+        if indent_config.switch_case then
+            options.SwitchCase = indent_config.switch_case
         end
-        if common.indent.variable_declarator then
-            options.VariableDeclarator = common.indent.variable_declarator
+        if indent_config.variable_declarator then
+            options.VariableDeclarator = indent_config.variable_declarator
         end
-        if common.indent.outer_iife_body then
-            options.outerIIFEBody = common.indent.outer_iife_body
+        if indent_config.outer_iife_body then
+            options.outerIIFEBody = indent_config.outer_iife_body
         end
-        if common.indent.member_expression then
-            options.MemberExpression = common.indent.member_expression
+        if indent_config.member_expression then
+            options.MemberExpression = indent_config.member_expression
         end
-        if common.indent.function_declaration then
+        if indent_config.function_declaration then
             options.FunctionDeclaration = {
-                body = common.indent.function_declaration.body,
-                parameters = common.indent.function_declaration.parameters
+                body = indent_config.function_declaration.body,
+                parameters = indent_config.function_declaration.parameters
             }
         end
-        if common.indent.function_expression then
+        if indent_config.function_expression then
             options.FunctionExpression = {
-                body = common.indent.function_expression.body,
-                parameters = common.indent.function_expression.parameters
+                body = indent_config.function_expression.body,
+                parameters = indent_config.function_expression.parameters
             }
         end
-        if common.indent.static_block then
-            options.StaticBlock = common.indent.static_block
+        if indent_config.static_block then
+            options.StaticBlock = indent_config.static_block
         end
-        if common.indent.call_expression then
+        if indent_config.call_expression then
             options.CallExpression = {
-                arguments = common.indent.call_expression.arguments,
+                arguments = indent_config.call_expression.arguments,
             }
         end
-        if common.indent.array_expression then
-            options.ArrayExpression = common.indent.array_expression
+        if indent_config.array_expression then
+            options.ArrayExpression = indent_config.array_expression
         end
-        if common.indent.object_expression then
-            options.ObjectExpression = common.indent.object_expression
+        if indent_config.object_expression then
+            options.ObjectExpression = indent_config.object_expression
         end
-        if common.indent.import_declaration then
-            options.ImportDeclaration = common.indent.import_declaration
+        if indent_config.import_declaration then
+            options.ImportDeclaration = indent_config.import_declaration
         end
-        if common.indent.flat_ternary_expressions ~= nil then
-            options.flatTernaryExpressions = common.indent.flat_ternary_expressions
+        if indent_config.flat_ternary_expressions ~= nil then
+            options.flatTernaryExpressions = indent_config.flat_ternary_expressions
         end
-        if common.indent.offset_ternary_expressions ~= nil then
-            options.offsetTernaryExpressions = common.indent.offset_ternary_expressions
+        if indent_config.offset_ternary_expressions ~= nil then
+            options.offsetTernaryExpressions = indent_config.offset_ternary_expressions
         end
-        if common.indent.ignore_nodes then
-            if type(common.indent.ignore_nodes) == "string" then
-                options.ignoredNodes = { common.indent.ignore_nodes }
+        if indent_config.ignore_nodes then
+            if type(indent_config.ignore_nodes) == "string" then
+                options.ignoredNodes = { indent_config.ignore_nodes }
             else
-                options.ignoredNodes = common.indent.ignore_nodes
+                options.ignoredNodes = indent_config.ignore_nodes
             end
         end
-        if common.indent.ignore_comments ~= nil then
-            options.ignoreComments = common.indent.ignore_comments
+        if indent_config.ignore_comments ~= nil then
+            options.ignoreComments = indent_config.ignore_comments
         end
 
         -- Add the options table as third parameter if any options were set
         if next(options) ~= nil then
             table.insert(indentOptions, options)
-        end
-
-        -- Special case for tabs with size
-        if indentation == "tabs" and indent_size then
-            table.insert(indentOptions, 2, indent_size)
-        end
-
+        end  
         return indentOptions
     end
 
+    --Function to map Flint rules to ESLint rules
+    local function mapFlintToESLint(flint_key, eslint_rule)
+        local value = common[flint_key]
+        if value ~= nil then
+            local plugin = getplugin(flint_key)
+            return {
+                [eslint_rule] = { "error", value },
+                [plugin .. "/" .. eslint_rule] = { "error", value }
+            }
+        end
+        return {}
+    end
+
+    --Define ESLint rules
     local rules = {
-        --Indentation rules
         indent = getIndentRule(common.indent),
+        semi = mapFlintToESLint("require_semicolons", "semi"),
+        quotes = mapFlintToESLint("quote_style", "quotes"),
+        ["max-len"] = mapFlintToESLint("max_line_length", "max-len"),
+        ["array-bracket-newline"] = mapFlintToESLint("array_bracket_newline_js", "array-bracket-newline"),
+        ["array-element-newline"] = mapFlintToESLint("array_element_newline_ts", "array-element-newline"),
+        ["no-trailing-spaces"] = mapFlintToESLint("trailing_whitespace", "no-trailing-spaces")
     }
+
+    -- Remove nil values
+    for key, value in pairs(rules) do
+        if value == nil then rules[key] = nil end
+    end
+
     -- Prepare the ESLint config as a Lua table
     local eslintConfig = {
         {
+            plugins = plugins,
             rules = rules,
-            plugins = plugins
         }
     }
 
+    -- Generate Import statements 
+    local importStatements = ""
+    for name, path in pairs(imports) do
+        importStatements = importStatements .. "import " .. name .. " from '" .. path .. "';\n"
+    end
 
     -- Convert the table to a JSON string
     return {
-        ["eslint.config.js"] = "export default " .. to_json(eslintConfig) .. ";"
+        ["eslint.config.js"] = importStatements ..
+            "\nexport default " .. to_json(eslintConfig) .. ";"
     }
 end
