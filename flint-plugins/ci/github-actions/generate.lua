836e5c96de130cb0c4badd239cfbb56970f66f41
Soham Karandikar <sohamk10@gmail.com>
2025-03-10 23:22:54 +0530

    Updated github actions plugin

--- flint-plugins/ci/github-actions/generate.lua
+++ flint-plugins/ci/github-actions/generate.lua
@@ -15,46 +15,65 @@ function Generate(config)
                         uses = "actions/checkout@v3"
                     },
                     {
-                        name = "Install Dependencies",
+                        name = "Install Git",
                         run = [[
-                            sudo apt-get update
-                            sudo apt-get install -y git curl build-essential
-                            curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
-                            source "$HOME/.cargo/env"
+                        sudo apt-get update
+                        sudo apt-get install -y git
                         ]]
                     },
                     {
-                        name = "Install Flint",
+                        name = "Install Node.js",
+                        uses = "actions/setup-node@v3",
+                        with = {
+                            ["node-version"] = "18.x"
+                        }
+                    },
+                    {
+                        name = "Install Jest and ESLint",
                         run = [[
-                            git clone https://github.com/skadewdl3/flint
-                            cd flint
-                            cargo build --release
-                            cp target/release/flint ../flint
-                            echo "$PWD" >> $GITHUB_PATH
-                            cd ..
-                            chmod +x ./flint
-                        ]]
+                                                npm install -g jest eslint --no-fund --no-audit --silent
+                                                echo "Node.js, Jest, and ESLint installed successfully"
+                                            ]]
+                    },
+                    {
+                        name = "Install Flint",
+                        uses = "robinraju/release-downloader@v1",
+                        with = {
+                            latest = true,
+                            repository = "skadewdl3/flint",
+                            ["out-file-path"] = ".",
+                            fileName = "flint",
+                            prerelease = true
+                        }
                     },
                     {
                         name = "Run Tests",
                         run = [[
+                            chmod +x ./flint
+                            ./flint install
                             ./flint test --test
-                            echo "Processing test results..."
                         ]]
                     },
                     {
                         name = "Upload Test Results",
-                        uses = "actions/upload-artifact@v3",
+                        uses = "actions/upload-artifact@v4",
                         with = {
-                            name = ".flint/reports/reports/report.json",
+                            name = "Test Results",
                             path = ".flint/reports/reports/report.json"
+                        }
+                    },
+                    {
+                        name = "Upload Logs",
+                        uses = "actions/upload-artifact@v4",
+                        with = {
+                            name = "Logs",
+                            path = "logs.txt",
                         }
                     }
                 }
             }
         }
     }
-
     return {
         ["workflows.yml"] = yaml.stringify(workflow)
     }
